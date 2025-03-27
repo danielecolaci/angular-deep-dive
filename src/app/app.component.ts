584456742c4528a08ec75ab293b1c3548417019a
Daniele Colaci <daniele.colaci@mindthepp.it>
2025-03-27 15:36:08 +0100

    Separate contents into several components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
+import { TrafficComponent } from "./dashboard/traffic/traffic.component";
+import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
+import { TicketsComponent } from "./dashboard/tickets/tickets.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, DashboardItemComponent, TrafficComponent, ServerStatusComponent, TicketsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
